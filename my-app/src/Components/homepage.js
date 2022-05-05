87194b0fecb25b1e8a348a4261d7a860c663b684
Diana Aliyeva <daliyeva@ne.virtmed.com>
2022-05-05 17:33:35 -0400

    homepage

---
+++ my-app/src/Components/homepage.js
@@ -0,0 +1,18 @@
+import './homepage.css';
+
+export const Homepage = () => {
+    return ( <div class="navbar">
+    <div class="dropdown">
+      <button class="dropbtn">Dropdown
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">All Plants</a>
+        <a href="#">Cart</a>
+      </div>
+    </div>
+  </div> );
+  };
+  
