be9338239a51d7970b38ca498176bbd8d149741c
andresanunezt <andresanunez98@gmail.com>
2022-05-05 16:53:59 -0400

    Plants updated

--- my-app/src/Components/Plant.js
+++ my-app/src/Components/Plant.js
@@ -1,26 +1,33 @@
 import './plant.css'
+import { plantsdata } from './plantdata';
+
+
 export const Plant = () => {
+    let plant1 = plantsdata[0];
+
   return (
     <div className='plant_page'>
     <div className="plant_page-container">
       <div className="plant_page-section-1">
-          <h1> Plant name</h1>
+          <h1> {plant1.name}</h1>
           <div className='plant-img-div'>
         <img
-          alt="Plant-name"
+          alt={plant1.name}
           className='plant-img'
-          src="https://www.johnnyseeds.com/dw/image/v2/BBBW_PRD/on/demandware.static/-/Sites-jss-master/default/dwaee182f3/images/products/flowers/01883_01_giantorange.jpg?sw=774&cx=302&cy=0&cw=1196&ch=1196"
+          src={plant1.image}
           size='300'
         /></div>
+        <p>{plant1.likes} likes</p>
         <div className='button-div'>
         <button> Like</button>
         <button>Add to Cart</button>
         </div>
          </div>
       <div className="plant_page-section-2">
-        <p className="plan_card-annual-price" color="white">
-          Text about Plant
+        <p className="plant_page-paragraph">
+         {plant1.description}
         </p>
+
       </div>
       </div>
     </div>
