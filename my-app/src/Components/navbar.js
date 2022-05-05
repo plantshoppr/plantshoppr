73e808f668756a013536b017df8b03ddfedee565
Diana Aliyeva <daliyeva@ne.virtmed.com>
2022-05-05 16:23:39 -0400

    navbar

--- my-app/src/Components/navbar.js
+++ my-app/src/Components/navbar.js
@@ -1,1 +1,18 @@
-<p>Home</p>
+import './navbar.css';
+
+export const Navbar = () => {
+  return (<div class="navbar">
+  <div class="dropdown">
+    <button class="dropbtn">Dropdown
+      <i class="fa fa-caret-down"></i>
+    </button>
+    <div class="dropdown-content">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">All Plants</a>
+      <a href="#">Cart</a>
+    </div>
+  </div>
+</div>
+);
+};
