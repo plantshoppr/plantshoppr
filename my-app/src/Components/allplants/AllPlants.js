afe2dfc4c067a8b385a3773e02f8ff2ae95413da
Constance Nwaigwe <constancenwaigwe@Constances-MacBook-Pro.local>
2022-06-16 12:24:09 -0400

    fixing div error

--- my-app/src/Components/allplants/AllPlants.js
+++ my-app/src/Components/allplants/AllPlants.js
@@ -13,15 +13,15 @@ const AllPlants = () => {
         {plantsdata.map((data) => {
           return (
             <div className="plants">
-            <Link className="link-plant" to={`/plant/${data.id}`}>
-              <img src={data.image} alt="img of plant" />
-              <div className="nameandprice">
-                <p>{data.name}</p>
-                <p>{data.price}</p>
-              </div>
-              <p>Likes: {data.likes}</p>
+              <Link className="link-plant" to={`/plant/${data.id}`}>
+                <img src={data.image} alt="img of plant" />
+                <div className="nameandprice">
+                  <p>{data.name}</p>
+                  <p>{data.price}</p>
+                </div>
+                <p>Likes: {data.likes}</p>
+              </Link>
             </div>
-            </Link>
           );
         })}
       </div>
