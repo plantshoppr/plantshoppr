30236f5eb22610102b89c43b788cc1ee5ba755cd
andresanunezt <andresanunez98@gmail.com>
2022-06-16 12:48:47 -0400

    Contact page + About page

---
+++ my-app/src/Components/about/index.js
@@ -0,0 +1,23 @@
+import "./styles.css";
+
+export const About = () => {
+  return (
+    <div className="about-container">
+      <h4>About</h4>
+      <p>
+        Do you know how to use you know? It can start, finish, or interject a
+        sentence. Native speakers might start with you know while we collect our
+        thoughts. We also say it to check for agreement. It's important to note
+        fluent English speakers use this when we assume the other person shares
+        the same knowledge or opinion as us.
+      </p>
+      <p>
+        Do you know how to use you know? It can start, finish, or interject a
+        sentence. Native speakers might start with you know while we collect our
+        thoughts. We also say it to check for agreement. It's important to note
+        fluent English speakers use this when we assume the other person shares
+        the same knowledge or opinion as us.
+      </p>
+    </div>
+  );
+};
