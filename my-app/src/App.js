71e6191f7777de1314a11437166699b0aac277e5
Constance Nwaigwe <constancenwaigwe@Constances-MacBook-Pro.local>
2022-06-30 16:27:11 -0400

    footer

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,15 +4,16 @@ import { AllPlants } from "./Components/allplants/AllPlants";
 import { Plant } from "./Components/Plant";
 import { Navbar } from "./Components/navbar";
 import { Homepage } from "./Components/homepage";
-import {About} from './Components/about'
+import { About } from "./Components/about";
 import { Contact } from "./Components/contact";
+import { Footer } from "./Components/footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar />
+        <Navbar />
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           <Route exact path="/all-plants" element={<AllPlants />} />
@@ -21,6 +22,7 @@ function App() {
           <Route exact path="/about" element={<About />} />
           <Route exact path="/contact" element={<Contact />} />
         </Routes>
+        <Footer />
       </Router>
     </div>
   );
